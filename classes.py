86dbb4c15c96f6060c7fc6c793e1eb5de90c715c
Emma <emmanuelaboagyesarfo@gmail.com>
2024-03-19 21:27:04 +0000

    initial commit

---
+++ classes.py
@@ -0,0 +1,11 @@
+class Person:
+
+    def __init__(self, name, gender):
+        self.name = name
+        self.gender = gender 
+    
+    # def __str__(self)
+    
+chloe = Person("Chloe", "Female")
+
+print(chloe)
